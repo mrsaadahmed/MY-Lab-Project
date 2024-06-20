a2ac707dd263cd3efc75fb46698a4ee1104528d3
Saad Ahmed Siddiqui <saadahmedsiddiqui5@gmail.com>
2024-06-20 05:59:12 +0500

    Done

--- splash.c
+++ splash.c
@@ -33,8 +33,8 @@ void Splash() {
     M_PlaySound("splash.wav");
 
     PrintLineWhite("You, a local artist captivated by Mohatta Palace's rich history, are granted a special permit to explore the museum after closing hours.  Legends whisper of a hidden room within the palace walls, said to hold forgotten artifacts and secrets of its past residents.  Armed with a borrowed lantern and a sketchpad, you delve into the dimly lit halls adorned with intricate carvings.  However, as the storm rages outside, the shadows within the palace take on menacing forms. Eerie whispers fill the air, and whispers morph into chilling wails. \n");
-    
-    M_StopSound();
+    PrintLineMagenta("Press enter to continue...\n");
+    getchar();
 
     Scene1();
 }
